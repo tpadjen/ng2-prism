b93a042e2e82632df8adde32cba35eb4c799f4a8
Tim Padjen <tpadjen@gmail.com>
2016-02-15 14:01:22 -0700

    Languages example

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,10 +6,15 @@ import {CodeblockComponent} from 'ng2-prism/codeblock';
 
 import 'ng2-prism/languages/prism-ruby';
 import 'ng2-prism/languages/prism-java';
+import 'ng2-prism/languages/prism-go';
+import 'ng2-prism/languages/prism-python';
 
 @Component({
   selector: 'ng2-prism-app',
   templateUrl: 'app/app.component.html',
+  styles: [`
+    p {padding-left: 14px;}
+  `],
   directives: [ExampleComponent, CodeblockComponent]
 })
 export class AppComponent {}
--- app/example/example.component.ts
+++ app/example/example.component.ts
@@ -3,31 +3,20 @@ import {Component, Input} from 'angular2/core';
 @Component({
   selector: 'example',
   template: `
-      <h1 class="title">{{title}}</h1>
-      <section class="content">
-        <div class="pre">
-          <ng-content select="[pre]"></ng-content>
+    <section class="content">
+      <div class="blocks">
+        <div class="col">
+          <h1>Template</h1>
+          <ng-content select="[code]"></ng-content>
         </div>
-        <div class="blocks">
-          <div class="col">
-            <h1>Template</h1>
-            <ng-content select="[code]"></ng-content>
-          </div>
-          <div class="col">
-            <h1>Result</h1>
-            <ng-content select="[highlighted]"></ng-content>
-          </div>
+        <div class="col">
+          <h1 class="result">Result</h1>
+          <ng-content select="[highlighted]"></ng-content>
         </div>
-        <div class="post">
-          <ng-content select="[post]"></ng-content>
-        </div>
-      </section>
+      </div>
+    </section>
   `,
   styleUrls: [`app/example/example.component.css`],
   directives: []
 })
-export class ExampleComponent {
-
-  @Input() title;
-
-}
+export class ExampleComponent { }
