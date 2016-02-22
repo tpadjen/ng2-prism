b6ab129e4b7957b32788ed8c3af871aea701688d
Tim Padjen <tpadjen@gmail.com>
2016-02-22 08:09:39 -0700

    Language directive template

---
+++ src/language.template.ts
@@ -0,0 +1,20 @@
+import {Directive, ElementRef} from 'angular2/core';
+
+import 'ng2-prism/languages/prism-{{lang}}';
+
+@Directive({
+  selector: 'codeblock[{{lang}}]'
+})
+export class {{lang_title}} {
+
+  codeblock:any;
+
+  constructor(private el:ElementRef) {  }
+
+  ngOnInit() {
+    // get the host
+    this.codeblock = (<any>this.el).internalElement.componentView.context;
+    this.codeblock.language = '{{lang}}';
+  }
+
+}
